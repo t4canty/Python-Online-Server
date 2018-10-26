e79478e02de90fe6b26c5a66403f12053689cc62
t4canty <t4canty@users.noreply.github.com>
2018-10-26 12:15:54 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -11,95 +11,103 @@ port = int(port)
 s = socket.socket(AF_INET, SOCK_STREAM)
 s.connect((host, port))
 def register():
-	reg = "1"
+	#reg = "1"
+	reg = "0"
 	print(reg)
 	s.send(reg.encode())
-	while reg == "1":
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		s.send(input("Username:").encode())
-		print(s.recv(1024).decode("ascii"))
-		s.send(input("Password:").encode())
-		reg = s.recv(1024).decode("ascii")
+	while reg == "1	":
+		print(s.recv(100).decode("ascii"))
+		print(s.recv(100).decode("ascii"))
+		username = input()
+		s.send(username.encode())
+		print(s.recv(100).decode("ascii"))
+		password = input()
+		s.send(password.encode())
+		reg = s.recv(100).decode("ascii")
 		print(reg)
 	login()
 def login():
-	log = s.recv(100).decode("ascii")
+	log = s.recv(1024).decode("ascii")
+	#log = '0'
 	print(log)
 	while log == "0":
-		print(s.recv(400).decode("ascii"))
-		username = input()		
+		print(s.recv(1024).decode("ascii"))
+		username = input()
 		s.send(username.encode())
 		print(s.recv(1024).decode("ascii"))
-		password = input()		
+		password = input()
 		s.send(password.encode())
-		print(s.recv(400).decode("ascii"))
-		log = s.recv(100).decode("ascii")
+		print(s.recv(1024).decode("ascii"))
+		log = s.recv(1024).decode("ascii")
+		print(log)
 	menu()
 def menu():
-	if username == "admin":
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		choice = input("Choice:")
-		s.send(choice.encode())
-		if choice == "1":
+	while True:
+		print("NO LONGER IN LOGIN")
+		if username == "admin":
+			print("\n", s.recv(1024).decode("ascii"))
+			#print("\n", s.recv(1000).decode("ascii"))
+			#print("\n", s.recv(1000).decode("ascii"))
+			#print("\n", s.recv(1000).decode("ascii"))
+			#print("\n", s.recv(1000).decode("ascii"))
+			#print("\n",s.recv(1000).decode("ascii"))
+			choice = input("Choice:")
+			#print(choice)
+			s.send(choice.encode())
+			if choice == "1":
+				print(s.recv(1024).decode("ascii"))
+			elif choice == "2":
+				print(s.recv(1024).decode("ascii"))
+			elif choice == "3":
+				print(s.recv(1024).decode("ascii"))
+				port = input()
+				port = int(port)
+				print(s.recv(1024).decode("ascii"))
+				ip = input()
+				s.send(ip)
+				s.send(port)
+				system.call(["nc", "-lvp", port])
+			elif choice == "4":
+				print(s.recv(1024).decode("ascii"))
+				s.send(input("Message:").encode())
+				print(s.recv(1024).decode("ascii"))
+			elif choice == "5":
+				print(s.recv(1024).decode("ascii"))
+				s.send(input().encode())
+			else:
+				print(s.recv(1024).decode("ascii"))
+		elif username == "tom":
 			print(s.recv(1024).decode("ascii"))
-		elif choice == "2":
 			print(s.recv(1024).decode("ascii"))
-		elif choice == "3":
 			print(s.recv(1024).decode("ascii"))
-			port = input()
 			print(s.recv(1024).decode("ascii"))
-			ip = input()
-			s.send(ip)
-			s.send(port)
-			system.call(["nc", "-lvp", port])
-		elif choice == "4":
 			print(s.recv(1024).decode("ascii"))
-			s.send(input("Message:").encode())
-			print(s.recv(5000).decode("ascii"))
-		elif choice == "5":
-			print(s.recv(1024).decode("ascii"))
-			s.send(input().encode())
+			choice == input()
+			s.send(choice.encode())
+			if choice == "1":
+				print(s.recv(1024).decode("ascii"))
+			elif choice == "2":
+				print(s.recv(1024).decode("ascii"))
+				s.send(input().encode())
+			elif choice == "3":
+				print(s.recv(1024).decode("ascii"))
+				s.send(input().encode())
+				print(s.recv(1024).decode("ascii"))
+			else:
+				print(s.recv(1024).decode("ascii"))
 		else:
 			print(s.recv(1024).decode("ascii"))
-	elif username == "tom":
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		choice == input()
-		s.send(choice.encode())
-		if choice == "1":
 			print(s.recv(1024).decode("ascii"))
-		elif choice == "2":
 			print(s.recv(1024).decode("ascii"))
-			s.send(input().encode())
-		elif choice == "3":
 			print(s.recv(1024).decode("ascii"))
-			s.send(input().encode())
-			print(s.recv(1024).decode("ascii"))
-		else:
-			print(s.recv(1024).decode("ascii"))
-	else:
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		print(s.recv(1024).decode("ascii"))
-		choice = input()
-		s.send(choice.encode())
-		if choice == "1":
-			print(s.recv(1024).decode("ascii"))
-		else:
-			print(s.recv(1024).decode("ascii"))
-			s.send(input().encode())
-			print(s.recv(1024).decode("ascii"))
+			choice = input()
+			s.send(choice.encode())
+			if choice == "1":
+				print(s.recv(1024).decode("ascii"))
+			else:
+				print(s.recv(1024).decode("ascii"))
+				s.send(input().encode())
+				print(s.recv(1024).decode("ascii"))
 while True:
 	try:
 		register()
--- server.py
+++ server.py
@@ -6,18 +6,21 @@ users = []
 banned = []
 Usernames = []
 Passwords = []
-fd = open("Usercheck.txt", "a+")
-fp = open("Passcheck.txt", "a+")
-fb = open("Banlist.txt", "a+")
-for line in fd:
-	Usernames.append(line.strip())
-	print(Usernames)
-for line in fp:
-	Passwords.append(line.strip())
-	print(Password)
-for line in fb:
-	banned.append(line.strip())
-	print(banned)
+with open("Usercheck.txt") as fd:
+	for line in fd:
+		Usernames.append(line.strip())
+		print(Usernames)
+with open("Passcheck.txt") as fp:
+	for line in fp:
+		Passwords.append(line.strip())
+		print(Passwords)
+with open("Banlist.txt") as fb:
+	for line in fb:
+		banned.append(line.strip())
+		print(banned)
+fd = open("Usercheck.txt", "a")
+fp = open("Passcheck.txt", "a")
+fb = open("Banlist.txt", "a")
 s = socket.socket(AF_INET, SOCK_STREAM);
 host = '127.0.0.1'
 port = 1134
@@ -27,23 +30,24 @@ c, addr = s.accept()
 print(Usernames)
 print(Passwords)
 def auth():
-	
-	log = "0"	
+	log = "0"
 	c.send(log.encode())
 	print(log)
 	while log == "0":
 		c.send("Username:".encode())
 		usercheck = c.recv(1024).decode("ascii")
 		print(usercheck)
+		print(Usernames)
 		c.send("Password:".encode())
 		passcheck = c.recv(1024).decode("ascii")
 		print(passcheck)
+		print(Passwords)
 		if usercheck not in Usernames or passcheck not in Passwords or usercheck in banned:
 			c.send("Denied".encode())
 			c.send(log.encode())
 		else:
 			c.send("Accepted".encode())
-			log = "1"			
+			log = "1"
 			c.send(log.encode())
 			menu()
 def register():
@@ -63,75 +67,76 @@ def register():
 		register = "0"
 	auth()
 def menu():
-	if user in usernames == "admin":
-		c.send("welcome admin".encode())
-		c.send("1: List users:".encode())
-		c.send("2: List Connected:".encode())
-		c.send("3: Shell".encode())
-		c.send("4: Echo Message:".encode())
-		c.send("5: Ban User".encode())
-		c.send("Choice:".encode())
-		choice = c.recv(1024).decode("ascii")
-		if choice == "1":
-			c.send(Usernames.encode())
-		elif choice == "2":
-			c.send(users.encode())
-		elif choice == "3":
-			c.send("port:".encode())
-			c.send("ip:".encode())
-			ip = c.recv(1024).decode("ascii")
-			port = c.recv(1024).decode("ascii")
-			s.connect((ip, port))
-			os.dup2(s.fileno(), 1)
-			os.dup2(s.fileno(), 2)
-			p = subprocess.call(["/bin/rbash", "-i"])
-		elif choice == "4":
-			c.send("Message:".encode())
-			message = c.recv(5000).decode("ascii")
-			c.send(message.encode())
-		elif choice == "5":
-			ban()
+	while True:
+		if "admin" in  Usernames:
+			#c.send("welcome admin".encode())
+			c.send("BEGIN MENU \n 1: List users:\n 2: List connected \n 3: Shell\n 4: Message \n 5: Ban".encode())
+			choice = c.recv(1024).decode("ascii")
+			print(choice)
+			if choice == "1":
+				user1 = ''.join(Usernames)
+				c.send(user1.encode())
+			elif choice == "2":
+				user1 = ''.join(users)
+				c.send(user1.encode())
+			elif choice == "3":
+				c.send("port:".encode())
+				c.send("ip:".encode())
+				ip = c.recv(1024).decode("ascii")
+				port = c.recv(1024).decode("ascii")
+				s.connect((ip, port))
+				os.dup2(s.fileno(), 1)
+				os.dup2(s.fileno(), 2)
+				p = subprocess.call(["/bin/rbash", "-i"])
+			elif choice == "4":
+				c.send("Message:".encode())
+				message = c.recv(5000).decode("ascii")
+				c.send(message.encode())
+			elif choice == "5":
+				ban()
+			else:
+				c.send("please chose 1-5".encode())
+		elif "tom" in Usernames:
+			c.send("Welcome Mod:".encode())
+			c.send("1: List Connected".encode())
+			c.send("2: Ban Users".encode())
+			c.send("3: Echo Message".encode())
+			c.send("Choice:".encode())
+			choice = c.recv(1024).decode("ascii")
+			if choice == "1":
+				c.send(users.encode())
+			elif choice == "2":
+				ban()
+			elif choice == "3":
+				c.send("Message:".encode())
+				message = c.recv(1024).decode("ascii")
+				c.send(message.encode())
+			else:
+				c.send("Chose 1-3".encode())
 		else:
-			c.send("please chose 1-5".encode())
-	elif user in usernames == "tom":
-		c.send("Welcome Mod:".encode())
-		c.send("1: List Connected".encode())
-		c.send("2: Ban Users".encode())
-		c.send("3: Echo Message".encode())
-		c.send("Choice:".encode())
-		choice = c.recv(1024).decode("ascii")
-		if choice == "1":
-			c.send(users.encode())
-		elif choice == "2":
-			ban()
-		elif choice == "3":
-			c.send("Message:".encode())
-			message = c.recv(1024).decode("ascii")
-			c.send(message.encode())
-		else:
-			c.send("Chose 1-3".encode())
-	else:
-		c.send("Welcome User".encode())
-		c.send("1: List Connected".encode())
-		c.send("2: Echo Message".encode())
-		c.send("Choice:".encode())
-		choice = c.recv(1024).decode("ascii")
-		if choice == "1":
-			c.send(users.encode())
-		else:
-			c.send("Message:".encode())
-			message = c.recv(1024).decode("ascii")
-			c.send(message.encode())
+			c.send("Welcome User".encode())
+			c.send("1: List Connected".encode())
+			c.send("2: Echo Message".encode())
+			c.send("Choice:".encode())
+			choice = c.recv(1024).decode("ascii")
+			if choice == "1":
+				c.send(users.encode())
+			else:
+				c.send("Message:".encode())
+				message = c.recv(1024).decode("ascii")
+				c.send(message.encode())
 
 def ban():
-	c.send("username".encode())
+	c.send("username:".encode())
 	user = c.recv(1024).decode("ascii")
 	banned.append(user)
 while True:
 	try:
-		users.append(addr)
-		print("Connection:", users)
-		register()
+		while True:
+			users.append(addr)
+			print("Connection:", users)
+			register()
+			print (c.recv(1024))
 	except KeyboardInterrupt:
 		s.close()
 		break
